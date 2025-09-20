f7f04d0380437bb21043bd802e9054444ca62dce
Taofik Dimas <158401549+taofikmmd@users.noreply.github.com>
2025-09-20 13:23:51 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -20,74 +20,74 @@ document.querySelectorAll('.btnDetail').forEach(item => {
         document.querySelector('.modalImage').appendChild(image);
         document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
         document.querySelector('.modalHarga').innerHTML = harga;
-        // const nohp = '6289654232550';
-        // let pesan = `https://api.whatsapp.com/send?phone={nohp}&text=Halo Kak Saya Pesan Produk ini ${gambar}`;
+        const nohp = '6289654232550';
+        let pesan = `https://api.whatsapp.com/send?phone={nohp}&text=Halo Kak Saya Pesan Produk ini ${gambar}`;
 
-        // document.querySelector('.btnBeli').href = pesan;
+        document.querySelector('.btnBeli').href = pesan;
         // Keranjang
 
 
 
     });
 });
-let cart = [];
-const cartItems = document.getElementById('cartItems');
-const checkoutBtn = document.getElementById('checkoutBtn');
-const nohp = "6289654232550"; // Ganti nomor WA
+// let cart = [];
+// const cartItems = document.getElementById('cartItems');
+// const checkoutBtn = document.getElementById('checkoutBtn');
+// const nohp = "6289654232550"; // Ganti nomor WA
 
-document.querySelectorAll('.add-to-cart').forEach(btn => {
-    btn.addEventListener('click', function () {
-        const name = this.dataset.name;
-        const price = this.dataset.price;
-        cart.push({ name, price });
-        renderCart();
-    });
-});
+// document.querySelectorAll('.add-to-cart').forEach(btn => {
+//     btn.addEventListener('click', function () {
+//         const name = this.dataset.name;
+//         const price = this.dataset.price;
+//         cart.push({ name, price });
+//         renderCart();
+//     });
+// });
 
-function renderCart() {
-    cartItems.innerHTML = '';
-    cart.forEach((item, index) => {
-        cartItems.innerHTML += `<li>${item.name} - Rp ${item.price} <button class="btn btn-sm btn-danger ms-2" onclick="removeItem(${index})">x</button></li>`;
-    });
-}
+// function renderCart() {
+//     cartItems.innerHTML = '';
+//     cart.forEach((item, index) => {
+//         cartItems.innerHTML += `<li>${item.name} - Rp ${item.price} <button class="btn btn-sm btn-danger ms-2" onclick="removeItem(${index})">x</button></li>`;
+//     });
+// }
 
-function removeItem(index) {
-    cart.splice(index, 1);
-    renderCart();
-}
+// function removeItem(index) {
+//     cart.splice(index, 1);
+//     renderCart();
+// }
 
-checkoutBtn.addEventListener('click', function () {
-    if (cart.length === 0) {
-        alert("Keranjang kosong!");
-        return;
-    }
-    let message = "Halo, saya ingin memesan:\n";
-    cart.forEach(item => {
-        message += `- ${item.name} (Rp ${item.price})\n`;
-    });
-    const url = `https://api.whatsapp.com/send?phone=${nohp}&text=${encodeURIComponent(message)}`;
-    window.open(url, '_blank');
-});
-document.getElementById("searchInput").addEventListener("keyup", function () {
-    let filter = this.value.toLowerCase();
-    let products = document.querySelectorAll(".product-card");
-    let found = false;
-    products.forEach(product => {
-        // Ambil semua teks di dalam kartu produk
-        let text = product.innerText.toLowerCase();
+// checkoutBtn.addEventListener('click', function () {
+//     if (cart.length === 0) {
+//         alert("Keranjang kosong!");
+//         return;
+//     }
+//     let message = "Halo, saya ingin memesan:\n";
+//     cart.forEach(item => {
+//         message += `- ${item.name} (Rp ${item.price})\n`;
+//     });
+//     const url = `https://api.whatsapp.com/send?phone=${nohp}&text=${encodeURIComponent(message)}`;
+//     window.open(url, '_blank');
+// });
+// document.getElementById("searchInput").addEventListener("keyup", function () {
+//     let filter = this.value.toLowerCase();
+//     let products = document.querySelectorAll(".product-card");
+//     let found = false;
+//     products.forEach(product => {
+//         // Ambil semua teks di dalam kartu produk
+//         let text = product.innerText.toLowerCase();
 
-        if (text.includes(filter)) {
-            product.style.display = "";
-        } else {
-            product.style.display = "none";
-        }
+//         if (text.includes(filter)) {
+//             product.style.display = "";
+//         } else {
+//             product.style.display = "none";
+//         }
 
-    });
-    // cek kalau tidak ada produk
-    const notFoundMessage = document.getElementById("notFoundMessage");
-    if (!found) {
-        notFoundMessage.style.display = "block";
-    } else {
-        notFoundMessage.style.display = "none";
-    }
-});
+//     });
+// cek kalau tidak ada produk
+// const notFoundMessage = document.getElementById("notFoundMessage");
+// if (!found) {
+//     notFoundMessage.style.display = "block";
+// } else {
+//     notFoundMessage.style.display = "none";
+// }
+// });
